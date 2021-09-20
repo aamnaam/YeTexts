6a9e14ec6cc88ed0bffc8bbd039fe2318bbbf990
aamnaam <aamaanmohammed17@gmail.com>
2021-09-21 00:09:40 +0100

    Add External API call on message

--- Backend/app.py
+++ Backend/app.py
@@ -1,12 +1,19 @@
 from flask import Flask, request, render_template
 from twilio.twiml.messaging_response import MessagingResponse
 from applying_users.models import ApplyingUser
+import requests
 
 app = Flask(__name__)
 
+kanye_api: str = 'https://api.kanye.rest'
+
+@app.route('/')
+def home() -> str:
+    return 'YeTexts'
+
 
 @app.route('/api/register', methods=['GET', 'POST'])
-def index():
+def index() -> str:
     if request.method == 'POST':
         user_details = request.form
         name = user_details['name']
@@ -15,14 +22,28 @@ def index():
 
 
 @app.route('/api/ye', methods=['POST'])
-def send_quote():
+def send_quote() -> str:
+    '''Returns preset response to a WhatsApp message'''
+
     msg = request.form.get('Body')
     sender_id = request.form.get('WaId')
-    # print(str(request.form))
     res = MessagingResponse()
-    res.message(f"You said: {str(request.form)}")
+    if msg == 'ye':
+        res.message(get_ye_quote()) 
+    else:
+        res.message(f"Command not found: '{msg}'")
     return str(res)
 
 
-if __name__ == '__main__':
+def get_ye_quote() -> str:
+    res = requests.get(kanye_api)
+    return res.json()['quote']
+
+
+def main() -> None:
     app.run()
+
+
+if __name__ == '__main__':
+    main()
+    
