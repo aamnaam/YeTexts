585c5d7075142bcdb1d210abfec2d3c94c549328
aamnaam <aamaanmohammed17@gmail.com>
2021-10-05 22:20:39 +0100

    Add Response to registration endpoint, user ID allocation, inspirational quote API support

--- api/app.py
+++ api/app.py
@@ -1,36 +1,38 @@
 import os
 from dotenv import load_dotenv
 import requests
-
-from flask import Flask, request, render_template
+from flask import Flask, request, Response
 from twilio.twiml.messaging_response import MessagingResponse
 from pymongo import MongoClient
 
-
 app = Flask(__name__)
-kanye_api: str = 'https://api.kanye.rest'
-cluster = MongoClient(os.environ.get("DB-CONNECTION"))
 
+kanye_endpoint = "https://api.kanye.rest"
+random_inspire_endpoint = "https://zenquotes.io/api/random"
+
+# MongoDB setup
+load_dotenv()
+cluster = MongoClient(os.environ.get("DB-CONNECTION"))
 db = cluster["ye_texts"]
 to_verify_collection = db["requesting_verification"]
+verified_users_collection = db["verified_users"]
 
-
-@app.route('/')
-def home() -> str:
-    return 'YeTexts'
+# to assign ID's to registering users for when they are verified
+num_users = verified_users_collection.count_documents({})
 
 
 @app.route('/', methods=['GET'])
 def index() -> str:
-    return render_template('index.html')
+    return "<h1>YeTexts<h1>"
 
 
 @app.route('/api/register', methods=['POST'])
-def register() -> None:
-    if request.method == 'POST':
-        user_details = request.json
-        name = user_details['name']
-        WAid = user_details['WAid']
+def register() -> Response:
+    """Receives registration data and adds to 'to_verify_collection'"""
+
+    user_details = request.json
+    name = user_details['name']
+    WAid = user_details['WAid']
 
     print(name + ' ' + WAid)
     new_user_data = {
@@ -41,6 +43,9 @@ def register() -> None:
 
     to_verify_collection.insert_one(new_user_data)
 
+    res = Response(status=200)
+    return res
+
 
 @app.route('/api/ye', methods=['POST'])
 def send_quote() -> str:
@@ -50,18 +55,27 @@ def send_quote() -> str:
     res = MessagingResponse()
     if msg == 'ye':
         res.message(get_ye_quote()) 
+    elif msg == 'inspire':
+        res.message(get_inspirational_quote())
     else:
         res.message(f"Command not found: '{msg}'")
     return str(res)
 
 
 def get_ye_quote() -> str:
-    res = requests.get(kanye_api)
+    res = requests.get(kanye_endpoint)
     return res.json()['quote']
 
 
+def get_inspirational_quote() -> str:
+    res = requests.get(random_inspire_endpoint)
+    return res.json()['q']
+
+
 def generate_user_id() -> int:
-    pass
+    global num_users
+    num_users += 1
+    return num_users
 
 
 def main() -> None:
